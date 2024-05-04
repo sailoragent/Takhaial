c414f30b3fdc3bbfd23c246ab8838ea1c4d6d83f
sailoragent <167790150+sailoragent@users.noreply.github.com>
2024-05-04 22:22:27 +0300

    Update Products.jsx

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -52,7 +52,7 @@ export default function Products(props) {
 
 
       <video autoPlay muted loop className="rotate-100 absolute  left-0 z-[-1] top-0 bottom-0 w-full h-full object-cover">
-        <source src={"/video/"+state.section5_video} type="video/mp4" />
+        <source src={state.section5_video} type="video/mp4" />
       </video>
     </section>
 
