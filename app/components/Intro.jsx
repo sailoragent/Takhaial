ee9102f6292afbec523f44ee1ace44c2e94e07ff
sailoragent <167790150+sailoragent@users.noreply.github.com>
2024-05-04 22:22:29 +0300

    Update Intro.jsx

--- app/components/Intro.jsx
+++ app/components/Intro.jsx
@@ -26,7 +26,7 @@ export default function Intro(props) {
     </div>
 
     <video autoPlay muted loop className="rotate-100 absolute  left-0 z-[-1] top-0 bottom-0 w-full h-full object-cover">
-      <source src={"/video/"+state.section1_video} type="video/mp4" />
+      <source src={state.section1_video} type="video/mp4" />
     </video>
   </section>);
 }
